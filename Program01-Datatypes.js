f85b199dc021fcb095c2c6c4fa677e18a1449365
MekalaPeriyannan <mekalaperiyannan@gmail.com>
2026-09-12 22:50:08 +0530

    Day 01

---
+++ Program01-Datatypes.js
@@ -0,0 +1,10 @@
+var Name = `Mekala`
+var age = 31
+var isMarried = true
+var value = null
+var value1
+console.log(Name)
+console.log(typeof age)
+console.log(typeof isMarried)
+console.log(typeof value)
+console.log(value1)
